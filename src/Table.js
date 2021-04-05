31a8a5e61f5e20708cf77446c278d30bc40b6ec7
Rahul Goyal <rahulxgoyal@gmail.com>
2021-04-05 00:15:39 -0700

    Add delete button

--- src/Table.js
+++ src/Table.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function TableHeader()  {
+function TableHeader() {
   return (
     <thead>
       <tr>
@@ -15,8 +15,11 @@ function TableBody(props) {
   const rows = props.characterData.map((row, index) => {
     return (
       <tr key={index}>
-        <td>{row.name}</td>         
+        <td>{row.name}</td>
         <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
       </tr>
     );
   });
@@ -31,7 +34,7 @@ function Table(props) {
   return (
     <table>
       <TableHeader />
-      <TableBody characterData={props.characterData} />
+      <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter} />
     </table>
   );
 }
