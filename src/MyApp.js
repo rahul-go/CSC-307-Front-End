672479074c999033dcd4630e7726fc44427b7778
Rahul Goyal <rahulxgoyal@gmail.com>
2021-04-11 05:49:32 -0700

    Update frontend with posted user response from backend

--- src/MyApp.js
+++ src/MyApp.js
@@ -47,7 +47,7 @@ function MyApp() {
   function updateList(person) {
     makePostCall(person).then(result => {
       if (result.status === 201)
-        setCharacters([...characters, person]);
+        setCharacters([...characters, result.data.user]);
     });
   }
 
